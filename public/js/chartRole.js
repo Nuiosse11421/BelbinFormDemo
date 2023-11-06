67634789fb4751a70df0625bfded5c4dd2d19862
Nuiosse11421 <notice.gameacc@gmail.com>
2023-11-06 21:05:24 +0700

    change style header

--- public/js/chartRole.js
+++ public/js/chartRole.js
@@ -5,12 +5,15 @@ function createMultiLineChart(timestamp, roleData) {
 
     // Role colors
     const roleColors = [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1',
+        'rgba(255, 0, 54, 1)',
+        'rgba(0, 140, 235, 1)',
+        'rgba(255, 181, 0, 1)',
+        'rgba(0, 192, 192, 1)',
+        'rgba(85, 10, 255, 1)',
+        'rgba(255, 127, 0, 1)',
+        'rgba(61, 48, 162, 1)',
+        'rgba(0, 169, 255, 1)',
+        'rgba(0, 0, 0, 1)',
     ];
 
     
